79852ed42336a18c0a2f17d1002adf3676bfc09f
shurma1 <dmitriy.prokhorov.04@mail.ru>
2025-11-14 00:44:29 +0300

    fix api

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -3,14 +3,13 @@ const isDev = import.meta.env.DEV;
 export const SSL: boolean = false;
 
 const HOST_PROD: string = "reassel.com";
-const PORT_PROD: number = 80;
 
 const HOST_DEV: string = "localhost";
 const PORT_DEV: number = 8000;
 
-export const PORT = isDev ? PORT_DEV : PORT_PROD;
+export const PORT = isDev ? `:${PORT_DEV}` : "";
 export const HOST = isDev ? HOST_DEV : HOST_PROD;
 
 const PROTOCOL = `http${isDev ? "" : "s"}://`;
 
-export const API_URL = `${PROTOCOL}${HOST}:${PORT}/api`;
+export const API_URL = `${PROTOCOL}${HOST}${PORT}/api`;
