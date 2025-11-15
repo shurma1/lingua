bdda52b44ec8a2cdb71e7d1dc12af3a15c513e88
Vlad1m1 <77707cx@gmail.com>
2025-11-15 04:09:10 +0300

    some changes

--- frontend/src/pages/LeaderboardPage/LeaderboardPage.tsx
+++ frontend/src/pages/LeaderboardPage/LeaderboardPage.tsx
@@ -1,22 +1,39 @@
+import { useState } from "react";
+
 import LeaderBoard from "@components/LeaderBoard";
 import Button from "@components/ui/Button";
+import WebApp from "@WebApp/WebApp";
+
+import { useFriendsMutations } from "@/hooks/useFriends";
+import { LeaderboardType } from "@/types/api";
 
 import styles from "./LeaderboardPage.module.scss";
 
 const LeaderboardPage = () => {
-	const handleClick = () => {
-		// Создаем экземпляр Audio с любым аудиофайлом
-		const audio = new Audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
-		
-		// Воспроизводим аудио
-		audio.play().catch((error) => {
-			console.error("Ошибка воспроизведения аудио:", error);
-		});
+	const [leaderboardType, setLeaderboardType] = useState<LeaderboardType>("all");
+	const { createInvite } = useFriendsMutations();
+
+	const handleAddFriend = async () => {
+		try {
+			const invite = await createInvite();
+			const inviteLink = `https://max.ru/t118_hakaton_bot?startApp=${invite.inviteId}`;
+			WebApp.shareMaxContent(
+				"Присоединись ко мне в Lingua! Вместе учим языки и соревнуемся",
+				inviteLink,
+			);
+		} catch (error) {
+			console.error("Failed to create invite:", error);
+		}
 	};
+
 	return (
 		<div className={styles.container}>
-			<Button onClick={handleClick} >кнопка</Button>
-			<LeaderBoard />
+            	<div className={styles.buttonWrapper}>
+				{leaderboardType === "friends" ? (
+					<Button mode="primary" appearance="contrast-static" className={styles.addFriendButton} style={{color: "#007AFFFF", justifyContent: "end"}} onClick={handleAddFriend}>Добавить друга</Button>
+				) : null}
+			</div>
+			<LeaderBoard onTypeChange={setLeaderboardType} />
 		</div>
 	);
 };
